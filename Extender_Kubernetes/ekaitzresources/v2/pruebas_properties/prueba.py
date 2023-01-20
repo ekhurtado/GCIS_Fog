8504098a178603a2437ec4ed98370f6a4d607e03
Ekaitz HURTADO <839073@adm.ehu.es>
2023-01-20 12:18:41 +0100

    Pruebas con archivo properties y estructura para v2 de entidad de aplicacion y componente

--- Extender_Kubernetes/ekaitzresources/v2/pruebas_properties/prueba.py
+++ Extender_Kubernetes/ekaitzresources/v2/pruebas_properties/prueba.py
@@ -14,3 +14,18 @@ print()
 #     print(key + ": " + config['OtraSection'][key])
 #
 # print(config.get('DatabaseSection', 'database.dbname'))
+
+key_received = "data-processing-app-1-model-2"
+
+
+if key_received in config['InformationSection'].values():
+    print("Pertenezco a alguna aplicacion")
+
+for key in config['OutTopicSection'].keys():
+    if key_received in key:
+        print("El topico de salida para la key que he recibido es: " + config['OutTopicSection'][key] +
+              " y pertenece al componente: " + key.split(".")[1])
+
+for key in config['MachineSection'].keys():
+    if key_received in key:
+        print("El limite de la maquina seleccionada es: " + config['MachineSection'][key])
