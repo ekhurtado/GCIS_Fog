ff9ce8beff36a6e0d34f741f3aedf9d64cc2c003
ekhurtado <e16hurtado@gmail.com>
2024-06-07 13:42:15 +0200

    Kubernetes dashboard setup.sh updated

--- Extender_Kubernetes/ekaitzresources/kubernetes_dashboard/setup.sh
+++ Extender_Kubernetes/ekaitzresources/kubernetes_dashboard/setup.sh
@@ -1,9 +1,12 @@
 # https://docs.k3s.io/installation/kube-dashboard
 
+sudo k3s kubectl create -f dashboard_namespace.yaml
+
 GITHUB_URL=https://github.com/kubernetes/dashboard/releases
 VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
-sudo k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
-
+#sudo k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+#sudo k3s kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v3.0.0-alpha0/charts/kubernetes-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 
 sleep 3 # espera 3 segundos a que se despliegue
 sudo k3s kubectl create -f dashboard.admin-user.yml -f dashboard.admin-user-role.yml
